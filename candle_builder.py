492741ce73b4e251a19f24fdd624d69298d340e2
rrajat9555 <bhartekadam@gmail.com>
2026-03-19 19:15:57 +0530

    candle_builder.py

--- candle_builder.py
+++ candle_builder.py
@@ -1,11 +1,23 @@
+import time
+
 class CandleBuilder:
 
-    def __init__(self):
+    def __init__(self, duration=30):  # 30 sec = demo (later 300 for 5 min)
         self.current_candle = None
         self.prev_candle = None
+        self.start_time = time.time()
+        self.duration = duration
 
     def update(self, price):
 
+        now = time.time()
+
+        # 🔥 Check if candle time is over
+        if now - self.start_time >= self.duration:
+            self.close_candle()
+            self.start_time = now
+
+        # Create new candle if none
         if not self.current_candle:
             self.current_candle = {
                 "open": price,
@@ -15,12 +27,14 @@ class CandleBuilder:
             }
             return
 
+        # Update current candle
         self.current_candle["high"] = max(self.current_candle["high"], price)
         self.current_candle["low"] = min(self.current_candle["low"], price)
         self.current_candle["close"] = price
 
     def close_candle(self):
-        self.prev_candle = self.current_candle
+        if self.current_candle:
+            self.prev_candle = self.current_candle
         self.current_candle = None
 
     def get_candles(self):
